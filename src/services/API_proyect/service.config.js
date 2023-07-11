a89ff79187ba1d0cfb815a0ba059c25fc61e0494
Igor <iluzarraga@gmail.com>
2023-07-11 11:41:35 +0200

    changed url for Backend

--- src/services/API_proyect/service.config.js
+++ src/services/API_proyect/service.config.js
@@ -10,7 +10,7 @@ const APIHeaders = {
 };
 
 export const API = axios.create({
-  baseURL: 'https://backend-dev-link-sepia.vercel.app/',
+  baseURL: 'https://backend-dev-link-sepia.vercel.app/api/v1',
   // baseURL: `http://localhost:8080/api/v1`,
   headers: APIHeaders,
   timeout: 60000,
