18a823e0eae402237c5e164304d1aabb93faf5f0
Igor <iluzarraga@gmail.com>
2023-07-11 11:05:56 +0200

    Froga

--- src/services/API_proyect/service.config.js
+++ src/services/API_proyect/service.config.js
@@ -10,7 +10,8 @@ const APIHeaders = {
 };
 
 export const API = axios.create({
-  baseURL: 'https://backend-dev-link-orcin.vercel.app/',
+  // baseURL: 'https://backend-dev-link-orcin.vercel.app/',
+  baseURL: 'https://backend-dev-link-orcin.vercel.app',
   // baseURL: `http://localhost:8080/api/v1`,
   headers: APIHeaders,
   timeout: 60000,
